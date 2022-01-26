45bc8abffc50048b3d3e4e925f81645b23da8784
Helen Ginn <github@hginn.co.uk>
2022-01-26 20:42:16 +0100

    new file

---
+++ src/print_file.cpp
@@ -0,0 +1,34 @@
+// vagabond
+// Copyright (C) 2019 Helen Ginn
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Please email: vagabond @ hginn.co.uk for more details.
+
+#include "FileReader.h"
+#include "Window.h"
+#include "FileManager.h"
+
+extern "C" 
+{
+
+int accept_file(char* file_path)
+{
+	FileManager *manager = Window::fileManager();
+	manager->acceptFile(file_path);
+
+	return 0;
+}
+
+}
