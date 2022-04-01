5a714cee7788e073576cbd8e2f053bb83730246b
Helen Ginn <github@hginn.co.uk>
2022-04-01 12:50:18 +0200

    Stepwise refinement covers greater range of atoms

--- vagabond/core/PositionRefinery.cpp
+++ vagabond/core/PositionRefinery.cpp
@@ -135,8 +135,10 @@ void PositionRefinery::stepwiseRefinement(AtomGroup *group)
 {
 	std::chrono::high_resolution_clock::time_point tstart;
 	tstart = std::chrono::high_resolution_clock::now();
+	
+	int nb = _calculator->sequenceHandler()->sequence(0)->blockCount() + 1;
 
-	for (size_t i = 0; i < _nBonds; i++)
+	for (size_t i = 0; i < nb; i++)
 	{
 		for (size_t j = 0; j < 5; j++)
 		{
