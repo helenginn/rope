8f1fee5054491dbb3acf7172c7204501264a6599
Helen Ginn <github@hginn.co.uk>
2022-06-16 16:45:42 +0200

    remove ttf.h import

--- vagabond/gui/elements/Library.cpp
+++ vagabond/gui/elements/Library.cpp
@@ -27,7 +27,7 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
+//#include <SDL2/SDL_ttf.h>
 #include <string>
 
 Library *Library::_library = NULL;
