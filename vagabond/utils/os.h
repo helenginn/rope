07c05bc8b8203d0a21e99fbf5f0e73e529a0ca2a
dtriand <64985525+dtriand@users.noreply.github.com>
2022-08-23 15:23:29 +0200

    OS precompiler instructions

---
+++ vagabond/utils/os.h
@@ -0,0 +1,35 @@
+//
+// Created by Dimitris on 23/08/2022.
+//
+
+#ifndef ROPE_OS_H
+#define ROPE_OS_H
+
+#ifdef _WIN64
+    #define OS_WINDOWS
+#elif _WIN32
+    #define OS_INVALID
+#elif __APPLE__
+#include "TargetConditionals.h"
+    #if TARGET_OS_IPHONE && TARGET_OS_SIMULATOR
+        // define something for simulator
+        // (although, checking for TARGET_OS_IPHONE should not be required).
+    #elif TARGET_OS_IPHONE && TARGET_OS_MACCATALYST
+        // define something for Mac's Catalyst
+    #elif TARGET_OS_IPHONE
+        // define something for iphone
+    #else
+        #define OS_MACOSX
+        #define OS_UNIX
+    #endif
+#elif __linux
+    #define OS_LINUX
+    #define OS_UNIX
+#elif __unix // all unices not caught above
+    #define OS_UNIX
+#elif __posix
+    #define OS_INVALID
+#endif
+
+
+#endif //ROPE_OS_H
