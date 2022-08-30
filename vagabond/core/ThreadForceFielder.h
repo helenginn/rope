729a2e1bd7a9c8e53b799bf92a21d6a0da8443a2
Helen Ginn <github@hginn.co.uk>
2022-08-30 14:04:50 +0200

    Add extra source files and meson.build entries

---
+++ vagabond/core/ThreadForceFielder.h
@@ -0,0 +1,43 @@
+// vagabond
+// Copyright (C) 2022 Helen Ginn
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Please email: vagabond @ hginn.co.uk for more details.
+
+#ifndef __vagabond__ThreadForceFielder__
+#define __vagabond__ThreadForceFielder__
+
+#include "ThreadWorker.h"
+
+class ForceFieldHandler;
+
+class ThreadForceFielder : public ThreadWorker
+{
+public:
+	ThreadForceFielder(ForceFieldHandler *h);
+	virtual ~ThreadForceFielder() {};
+	
+	virtual void start();
+
+	virtual std::string type()
+	{
+		return "Force field evaluator";
+	}
+private:
+	ForceFieldHandler *_ffHandler;
+
+};
+
+#endif
