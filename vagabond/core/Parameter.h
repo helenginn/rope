72ff5613501211e6fb9871f23d5c13313b3c04f7
Helen Ginn <github@hginn.co.uk>
2023-02-23 13:56:32 +0100

    Add Parameter.h header

---
+++ vagabond/core/Parameter.h
@@ -0,0 +1,44 @@
+// vagabond
+// Copyright (C) 2022 Helen Ginn
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Please email: vagabond @ hginn.co.uk for more details.
+
+#ifndef __vagabond__Parameter__
+#define __vagabond__Parameter__
+
+#include "ResidueId.h"
+#include "Bondstraint.h"
+
+class Parameter : public Bondstraint
+{
+public:
+	virtual ~Parameter() {};
+	virtual const double value() = 0;
+	virtual void setValue(const double value) = 0;
+	
+	virtual bool hasDesc(std::string desc) const = 0;
+	
+	virtual bool isTorsion() const
+	{
+		return false;
+	}
+
+	virtual bool isConstrained() const = 0;
+	virtual bool coversMainChain() = 0;
+	virtual const ResidueId residueId() = 0;
+};
+
+#endif
