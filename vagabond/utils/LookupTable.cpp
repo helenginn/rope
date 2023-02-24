bebd339e18c64782c01cd57f128f7d4bcc75dd6f
Helen Ginn <github@hginn.co.uk>
2023-02-24 16:18:30 +0100

    Add LookupTable.cpp/h and meson compile list

---
+++ vagabond/utils/LookupTable.cpp
@@ -0,0 +1,35 @@
+// vagabond
+// Copyright (C) 2022 Helen Ginn
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Please email: vagabond @ hginn.co.uk for more details.
+
+#include "LookupTable.h"
+
+LookupTable::LookupTable(const LookupMap &map)
+{
+	_map = map;
+}
+
+LookupTable LookupTable::defaultTable()
+{
+	LookupMap map;
+	map[0][0] = -2.f;
+	map[0][1] = +4.f;
+	map[1][0] = +0.f;
+	map[1][1] = +6.f;
+	LookupTable table(map);
+	return table;
+}
