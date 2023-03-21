25c0402426bd31e687591094b81d68d1546ed12e
Helen Ginn <github@hginn.co.uk>
2023-03-21 18:01:37 +0100

    send mat4, mat3, vec3 and vec4 directly to stream

--- vagabond/utils/glm_import.h
+++ vagabond/utils/glm_import.h
@@ -11,8 +11,41 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/string_cast.hpp>
 #include <glm/gtx/vector_angle.hpp>
+#include <ostream>
 
 #include "degrad.h"
+
+inline std::ostream &operator<<(std::ostream& stream, const glm::mat4x4& a)
+{
+	for (size_t i = 0; i < 4; i++)
+	{
+		for (size_t j = 0; j < 4; j++)
+		{
+			stream << a[i][j] << " ";
+		}
+		stream << std::endl;
+	}
+//	stream << glm::to_string(a);
+	return stream;
+}
+
+inline std::ostream &operator<<(std::ostream& stream, const glm::mat3x3& a)
+{
+	stream << glm::to_string(a);
+	return stream;
+}
+
+inline std::ostream &operator<<(std::ostream& stream, const glm::vec3& a)
+{
+	stream << glm::to_string(a);
+	return stream;
+}
+
+inline std::ostream &operator<<(std::ostream& stream, const glm::vec4& a)
+{
+	stream << glm::to_string(a);
+	return stream;
+}
 
 #endif
 
