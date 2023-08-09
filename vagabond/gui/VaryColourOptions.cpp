9d19c93b296843a0c6b39e3207a59f69b2e9270f
illiarius <romain.vin100@gmail.com>
2023-08-09 11:53:43 +0200

    Suppression of two useless tags in VaryColourOptions.cpp #13

--- vagabond/gui/VaryColourOptions.cpp
+++ vagabond/gui/VaryColourOptions.cpp
@@ -45,7 +45,6 @@ void VaryColourOptions::setup()
 
 		TextEntry *t = new TextEntry(str, this);
 		t->setValidationType(TextEntry::Numeric);
-		t->setReturnTag("min");
 		t->setRight(0.2, 0.7);
 		_min = t;
 		addObject(t);
@@ -56,7 +55,6 @@ void VaryColourOptions::setup()
 
 		TextEntry *t = new TextEntry(str, this);
 		t->setValidationType(TextEntry::Numeric);
-		t->setReturnTag("max");
 		t->setRight(0.8, 0.7);
 		_max = t;
 		addObject(t);
