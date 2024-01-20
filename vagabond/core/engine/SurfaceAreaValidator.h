46403c98b6427b24fe964be13c957f89b17f10f4
Iason Kourouklis <96156520+arg0n4ut@users.noreply.github.com>
2024-01-20 22:24:46 +0100

    working path evaluation

--- vagabond/core/engine/SurfaceAreaValidator.h
+++ vagabond/core/engine/SurfaceAreaValidator.h
@@ -1,6 +1,13 @@
 #include "AreaMeasurer.h"
+#include "BondCalculator.h"
+#include "BondSequenceHandler.h"
+#include "BaseTask.h"
+#include "Task.h"
+#include "Tasks.h"
+#include "Result.h"
 #include "vagabond/core/Environment.h"
-
+#include "gemmi/model.hpp"
+#include "vagabond/core/files/PdbFile.h"
 class SurfaceAreaValidator
 {
 public:
