9ae3663d483cb52de3359cce0624b95432c7b706
Iason Kourouklis <96156520+arg0n4ut@users.noreply.github.com>
2024-01-20 23:16:57 +0100

    validate_paths_1

--- vagabond/core/tests/test_surfaceareavalidator.cpp
+++ vagabond/core/tests/test_surfaceareavalidator.cpp
@@ -5,7 +5,8 @@ namespace tt = boost::test_tools;
 
 BOOST_AUTO_TEST_CASE(validate_paths_1)
 {
+  std::cout << std::endl << "validate_paths_1" << std::endl << std::endl;
   SurfaceAreaValidator SAV;
   SAV.loadEnv("rope.json");
-  SAV.validatePaths();
+  SAV.validatePaths(50);
 }
