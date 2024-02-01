551644487c966af32dfb32f0a769f4fa02df7c32
Iason Kourouklis <96156520+arg0n4ut@users.noreply.github.com>
2024-02-01 02:13:18 +0100

    validate_paths_2

--- vagabond/core/tests/test_surfaceareavalidator.cpp
+++ vagabond/core/tests/test_surfaceareavalidator.cpp
@@ -10,3 +10,11 @@ BOOST_AUTO_TEST_CASE(validate_paths_1)
   SAV.loadEnv("rope.json");
   SAV.validatePaths(100);
 }
+
+BOOST_AUTO_TEST_CASE(validate_paths_2)
+{
+  std::cout << std::endl << "validate_paths_2" << std::endl << std::endl;
+  SurfaceAreaValidator SAV;
+  SAV.loadEnv("rope_unrefined_paths.json");
+  SAV.validatePaths(100);
+}
