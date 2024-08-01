2365be383c792f75bc08eb8612526be69d7c7492
kboflynn <katie.oflynn@desy.de>
2024-08-01 11:45:34 +0200

    Thermodynamics scene added

---
+++ vagabond/gui/elements/PathThermodynamics.h
@@ -0,0 +1,37 @@
+// vagabond
+// Copyright (C) 2022 Helen Ginn
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Please email: vagabond @ hginn.co.uk for more details.
+
+#ifndef __vagabond__PathThermodynamics__
+#define __vagabond__PathThermodynamics__
+
+#include <vagabond/gui/elements/Scene.h>
+
+class PathGroup;
+class TextEntry;
+class Text;
+
+class PathThermodynamics : public Scene
+{
+public:
+	PathThermodynamics(Scene *prev, PathGroup *group);
+
+	virtual void setup();
+	virtual void buttonPressed(std::string tag, Button *button);
+};
+
+#endif
