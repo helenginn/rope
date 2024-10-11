16b68a60d8e5ef2c59198e4250f2da06c9abb7c9
virginia4 <apostolop.virginia@gmail.com>
2024-10-11 18:10:47 +0200

    - Calculate SVD matrices - extract flexibility "weigths" from V matrix and align them with the correct torsion - New function: addMultipleHBonds(), calculate the Jacobian matrix only after you have all the input HBonds

--- vagabond/gui/FlexibilityView.cpp
+++ vagabond/gui/FlexibilityView.cpp
@@ -1,6 +1,9 @@
 #include "FlexibilityView.h"
 
 #include <vagabond/gui/elements/Slider.h>
+#include <vagabond/gui/elements/Menu.h>
+#include <vagabond/gui/elements/TextButton.h>
+#include <vagabond/gui/elements/AskForText.h>
 
 #include <vagabond/core/Instance.h>
 #include <vagabond/core/AtomGroup.h>
@@ -14,7 +17,8 @@ FlexibilityView::FlexibilityView(Scene *prev, Instance *inst, Flexibility *flex)
 {
 	_flex = flex;
 	_instance = inst;
-	// setup();
+	_instance->load();
+	setPingPong(true);
 }
 
 FlexibilityView::~FlexibilityView()
@@ -23,12 +27,49 @@ FlexibilityView::~FlexibilityView()
 	_instance->unload();
 }
 
+void FlexibilityView::makeMenu()
+{
+	TextButton *text = new TextButton("Menu", this);
+	text->setReturnTag("menu");
+	text->setRight(0.95, 0.1);
+	addObject(text);
+}
+
+void FlexibilityView::buttonPressed(std::string tag, Button *button)
+{
+	if (tag == "menu")
+	{
+		glm::vec2 c = button->xy();
+		Menu *m = new Menu(this, this, "options");
+		m->addOption("Save state as PDB", "save_state");
+		m->setup(c.x, c.y);
+		setModal(m);
+	}
+	else if (tag == "options_save_state")
+	{
+		AskForText *aft = new AskForText(this, "PDB file name to save to:",
+		                                 "export_pdb", this);
+		setModal(aft);
+	}
+	else if (tag == "export_pdb")
+	{
+		TextEntry *te = static_cast<TextEntry *>(button);
+		std::string filename = te->scratch();
+
+		std::string path = getPath(filename);
+		std::string file = getFilename(filename);
+		check_path_and_make(path);
+
+		_instance->currentAtoms()->writeToFile(filename);
+	}
+	Display::buttonPressed(tag, button);
+}
+
 
 void FlexibilityView::setup()
 {
 	AtomGroup *grp = _instance->currentAtoms();
 	grp->recalculate();
-	
 	DisplayUnit *unit = new DisplayUnit(this);
 	// unit->loadAtoms(grp, _instance->entity());
 	unit->loadAtoms(grp, _instance->entity());
@@ -37,20 +78,19 @@ void FlexibilityView::setup()
 
 	Display::setup();
 	_flex->prepareResources();
+	setupSlider();
 	_flex->submitJobAndRetrieve(0.0);
-	_flex->submitJobAndRetrieve(0.0);
-
-
+	// _flex->submitJobAndRetrieve(0.0);
+	makeMenu();
 	// Create a vector of donor-acceptor pairs
     std::vector<std::pair<std::string, std::string>> donorAcceptorPairs = 
     {
         {"A-ILE3:N", "A-ASP32:O"},
         {"A-TYR22:N", "A-SER18:O"}
     };
-    callAddHBonds(donorAcceptorPairs);
+    _flex->addMultipleHBonds(donorAcceptorPairs);
+    // callAddHBonds(donorAcceptorPairs);
 	_flex->printHBonds();
-	setupSlider();
-	// askForAtomFlexibility();
 }
 
 void FlexibilityView::callAddHBonds(const std::vector<std::pair<std::string, std::string>> &donorAcceptorPairs) 
@@ -59,15 +99,7 @@ void FlexibilityView::callAddHBonds(const std::vector<std::pair<std::string, std
 	{
 		std::cout << "Calling callAddHbonds..." << std::endl;
     	_flex->addHBond(pair.first, pair.second);
-    	// before turning vector into a set I want to put together all the the vectors into one
     }
-
-    // Ask tp build the Jacobian matrix
-
-    // caclulate number of columns
-    int colJac = donorAcceptorPairs.size();
-
-    // calculate number of rows
 }
 
 void FlexibilityView::setupSlider()
