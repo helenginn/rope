ef729129e4743230ac4ca830cbc6913c04531738
virginia4 <apostolop.virginia@gmail.com>
2024-10-22 16:10:47 +0200

    Similar to CSVFile but for txt-format files

---
+++ vagabond/core/files/TextFile.h
@@ -0,0 +1,38 @@
+// vagabond
+// Copyright (C) 2022 Helen Ginn
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 
+// Please email: vagabond @ hginn.co.uk for more details.
+
+#ifndef __vagabond__TextFile__
+#define __vagabond__TextFile__
+
+#include "File.h"
+
+class TextFile : public File
+{
+public:
+	TextFile(std::string filename = "");
+
+	virtual File::Type cursoryLook();
+	virtual void parse();
+private:
+	void parseFileContents();
+	void processLine(std::string line);
+	void processHeaders(std::string line);
+	std::vector<std::string> _headers;
+};
+
+#endif
