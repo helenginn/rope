42c89915a447c0274d37433b70d0551983505580
Helen Ginn <github@hginn.co.uk>
2024-12-03 13:21:44 +0100

    histogram can return a mid_bin value

--- vagabond/utils/histogram.h
+++ vagabond/utils/histogram.h
@@ -6,17 +6,25 @@
 #include <float.h>
 #include <climits>
 
-struct Histogram
+template <typename T>
+struct Distribution
 {
 	float min = FLT_MAX;
 	float max = -FLT_MAX;
 	float step = 1;
 	
+	float mid_bin(int i)
+	{
+		return min + (i + 0.5) * step;
+	}
+	
 	int highest = 0;
 	int lowest = INT_MAX;
 
-	std::vector<int> freqs;
+	std::vector<T> freqs;
 };
+
+typedef Distribution<int> Histogram;
 
 void histogram(const std::vector<float> &vals, Histogram &hist);
 void get_histogram_limits(const std::vector<float> &vals, Histogram &hist);
