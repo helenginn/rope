1c568dd9539b7bcd3022d98b1e0f223892b9a340
kboflynn <katie.oflynn@desy.de>
2025-05-13 10:54:24 +0200

    Added slider to utilise multiple paths for calculations

--- vagabond/gui/PathThermodynamics.cpp
+++ vagabond/gui/PathThermodynamics.cpp
@@ -35,6 +35,8 @@
 #include <vagabond/gui/elements/ChooseRange.h>
 #include <vagabond/gui/elements/BadChoice.h>
 
+int pathNum = 0;
+
 PathThermodynamics::PathThermodynamics(Scene *prev, Entity *entity, const std::vector<PathGroup> &paths) : Scene(prev)
 {
 	_entity = entity;
@@ -52,6 +54,25 @@ void PathThermodynamics::setup()
 {
 	addTitle("Calculate path thermodynamics");
 	float top = 0.3;
+
+	if (_paths.size())
+	{
+		//PathGroup &group = _paths[0];
+		//const std::string mod_id = group[0]->startInstance()->model_id();
+		
+		if (pathNum < 1)
+		{
+			std::string str = "Choose number of paths (\"frames\") to utilise";
+			ChooseRange *cr = new ChooseRange(this, str, "choose_paths", this);
+			cr->setDefault(1);
+			cr->setRange(1, _paths.size(), _paths.size()-1);
+			setModal(cr);
+	
+			float num = cr->max();
+			int pathNum = lrint(num);
+		}
+	}
+
 
 	{
 		Text *t = new Text("Calculate single-structure entropy");
@@ -75,34 +96,26 @@ void PathThermodynamics::buttonPressed(std::string tag, Button *button)
 	{
 		struct Flag_par flag_par;
 		struct Entropy entropy;
-
+		
 		Sequence *seq = _entity->sequence();
 
 		PathEntropy path_entropy;
 
-		if (_paths.size())
-		{
-			PathGroup &group = _paths[0];
-			const std::string mod_id = group[0]->startInstance()->model_id();
-			std::cout << "Model ID: " << mod_id << std::endl;
-		}
-
 		std::cout << "Out of if statement..." << std::endl;
 		const std::string mod_id = _paths[0].front()->startInstance()->model_id();
-		std::cout << mod_id << std::endl;
 
 		Tors_res4nn* tors_res = new Tors_res4nn[seq->size()]{};
 
 		path_entropy.init_flag_par(&flag_par);	
 		path_entropy.get_atoms_and_residues(mod_id, *tors_res);
 
-		path_entropy.calculate_entropy_independent(1, flag_par, seq, tors_res, &entropy);
+		path_entropy.calculate_entropy_independent(pathNum, flag_par, seq, tors_res, &entropy);
 		std::cout << "entropy calculated" << std::endl;
 
 		{
-			std::string str = std::to_string(*(double *)entropy.total);
+			std::string str = "Total: " +  std::to_string(*(double *)entropy.total) + " (R units)\n" + "Per residue: " + std::to_string(*(double *)entropy.total/seq->size()) + " (R units)";
 			Text *t = new Text(str);
-			t->setRight(0.4, 0.6);
+			t->setLeft(0.2, 0.6);
 			addTempObject(t);
 		}
 	}
