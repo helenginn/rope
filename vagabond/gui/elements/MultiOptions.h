5d86e14821fbb287be739b09f84ea7fa7af87270
kboflynn <katie.oflynn@desy.de>
2025-12-08 13:29:23 +0100

    MultiOptions widget addition

---
+++ vagabond/gui/elements/MultiOptions.h
@@ -0,0 +1,18 @@
+#ifndef __vagabond__MultiOptions__
+#define __vagabond__MultiOptions__
+
+#include "Modal.h"
+#include "ButtonResponder.h"
+
+class MultiOptions : public Modal
+{
+public:
+    
+
+private:
+    ButtonResponder *_sender;
+    std::string _tag;
+};
+
+#endif
+~        
