6c31292978e1f2a115fd3a44084a0d0222568b54
kboflynn <katie.oflynn@desy.de>
2025-12-16 14:07:41 +0100

    Adding options to multi widget

--- vagabond/gui/elements/MultiOptions.h
+++ vagabond/gui/elements/MultiOptions.h
@@ -7,7 +7,13 @@
 class MultiOptions : public Modal
 {
 public:
-    
+    MultiOptions(Scene *scene, std::string text, std::string tag, ButtonResponder *sender);
+
+    void addYesNo(std::string ynText);
+
+    void addSlider(std::string sliderText, float min, float max);
+
+    virtual void buttonPressed(std::string tag, Button *button);
 
 private:
     ButtonResponder *_sender;
