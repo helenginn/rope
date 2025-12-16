36ff05f1405b5e087b466db66543777e22deb249
kboflynn <katie.oflynn@desy.de>
2025-12-16 14:07:41 +0100

    Adding options to multi widget

--- vagabond/gui/elements/MultiOptions.cpp
+++ vagabond/gui/elements/MultiOptions.cpp
@@ -1,1 +1,28 @@
 #include "MultiOptions.h"
+#include "AskYesNo.h"
+#include "ChooseRange.h"
+
+MultiOptions::MultiOptions(Scene *scene, std::string text, std::string tag, ButtonResponder *sender) : Modal(scene, 0.6, 0.4)
+{
+    Text *t = new Text(text);
+    t->resize(0.8);
+    t->setCentre(0.5, 0.35);
+    addObject(t);
+
+    _sender = sender;
+    _tag = tag;
+
+    setDismissable(true);
+}
+
+void MultiOptions::addYesNo(std::string ynText)
+{
+    AskYesNo *ayn = new AskYesNo(this, std::string ynText, "mist", sender);
+}
+
+void MultiOptions::addSlider(std::string sliderText, float min, float max)
+{
+    ChooseRange *cr = new ChooseRange(this, sliderText, tag + "_num_paths", sender);
+    cr->setDefault(min);
+    cr->setRange(min, max, min-max);
+}
