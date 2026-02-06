a79f297e62565c5ac112473404e054951b04de94
kboflynn <katie.oflynn@desy.de>
2026-02-06 13:33:38 +0100

    Began EntropyMetrics page

---
+++ vagabond/gui/EntropyMetrics.cpp
@@ -0,0 +1,11 @@
+#include "EntropyMetrics.h"
+
+EntropyMetrics::EntropyMetrics(Scene *prev) : Scene (prev)
+{
+
+}
+
+void EntropyMetrics::setup()
+{
+    addTitle("Select parameters for calculation");
+}
---
+++ vagabond/gui/EntropyMetrics.h
@@ -0,0 +1,15 @@
+#ifndef __vagabond__EntropyMetrics__
+#define __vagabond__EntropyMetrics__
+
+#include <vagabond/gui/elements/Scene.h>
+
+class EntropyMetrics : public Scene
+{
+public:
+    EntropyMetrics(Scene *scene);
+
+    virtual void setup();
+
+};
+
+#endif
