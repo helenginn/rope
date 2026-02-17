24aa6d0cf5f2b729436ae6603652467d785635bf
Helen Ginn <github@hginn.co.uk>
2026-02-17 16:05:13 +0100

    Rejigging MultiEngine to take engine type as template

--- vagabond/core/MultiEngine.h
+++ vagabond/core/MultiEngine.h
@@ -19,40 +19,356 @@
 #ifndef __vagabond__MultiEngine__
 #define __vagabond__MultiEngine__
 
-#include <engine/Task.h>
+#include "engine/ExpectantPhore.h"
+#include "SimplexEngine.h"
+#include "ResidueId.h"
+#include <vagabond/utils/Vec3s.h>
+#include <queue>
+#include <atomic>
 
-class Engine;
-class Tasks;
-class BaseTask;
+#include "MultiEngineBase.h"
 
-class MultiEngine
+template <class Key, class EngineType> class MultiEngine;
+
+template <class Key, class EngineType>
+class RunsMultiEngine
 {
 public:
-	void addImmediateTask(BaseTask *bt);
-	void addHangingTask(BaseTask *bt);
-	
-	Tasks *const &immediate() const
+	virtual std::map<Key, float> 
+	getMultiResult(const std::vector<float> &all, 
+	               MultiEngine<Key, EngineType> *caller) = 0;
+
+	virtual void finishedKey(const Key &key) {};
+
+};
+
+template <class Key, class EngineType>
+class MultiEngine : public RunsEngine, public MultiEngineBase
+{
+public:
+	MultiEngine(RunsMultiEngine<Key, EngineType> *caller, int param_num)
 	{
-		return _immediate;
+		_num = param_num;
+		_ref = caller;
+	}
+	
+	~MultiEngine()
+	{
+		do_on_each_engine([](Engine *engine) { delete engine; });
+	}
+	
+	void setMaxRuns(int mr)
+	{
+		_maxRuns = mr;
 	}
 
-	Tasks *const &hanging() const
+	// which key is sensitive to which parameters
+	void supplyInfo(const std::map<Key, std::vector<int>> &crucial_info)
 	{
-		return _hanging;
+		for (auto it = crucial_info.begin(); it != crucial_info.end(); it++)
+		{
+			if (it->second.size() == 0)
+			{
+				continue;
+			}
+
+			_key2Info[it->first] += it->second;
+			for (int idx : it->second)
+			{
+				if (idx > _num - 1)
+				{
+					_num = idx + 1;
+				}
+			}
+
+			if (!_key2Info.at(it->first).engine)
+			{
+				EngineType *engine = new EngineType(this);
+				engine->setMaxRuns(_maxRuns);
+				engine->setStepSize(_step);
+				_key2Info.at(it->first).engine = engine;
+				_engine2Key[engine] = it->first;
+				_key2Info.at(it->first).expected.expect_one();
+			}
+		}
+	}
+
+	virtual size_t parameterCount(Engine *caller)
+	{
+		if (_engine2Key.count(caller) == 0)
+		{
+			return 0; // will be updated later
+		}
+		Key &key = _engine2Key.at(caller);
+		return _key2Info.at(key).idxs.size();
 	}
 	
-	void clearTasks();
-
-	virtual void declareDone(Engine *sender, const std::vector<float> &best) = 0;
-protected:
-	MultiEngine();
-	~MultiEngine();
+	template <typename Func>
+	void do_on_each_engine(const Func &func)
+	{
+		for (auto it = _key2Info.begin(); it != _key2Info.end(); it++)
+		{
+			if (it->second.engine)
+			{
+				func(it->second.engine);
+			}
+		}
+	}
 	
-	Tasks *_immediate = nullptr;
-	Tasks *_hanging = nullptr;
-	int _threads = 4;
+	void setStepSize(float step)
+	{
+		_step = step;
+	}
+	
+	void cleanup()
+	{
+		clearTasks();
 
+		for (auto it = _key2Info.begin(); it != _key2Info.end(); it++)
+		{
+			it->second.cleanup();
+		}
+		
+		_received = 0;
+		_done = 0;
+	}
+
+	virtual void run()
+	{
+		cleanup();
+
+		std::unique_lock<std::mutex> lock(_result);
+		std::vector<Task<void *, void *> *> firsts;
+		auto start_engine = [this, &firsts](EngineType *engine)
+		{
+			auto *first = engine->taskedRun(this);
+			firsts.push_back(first);
+		};
+
+		do_on_each_engine(start_engine);
+
+		auto *mother_task = new Task<void *, void *>([](void *)
+		                                             { return nullptr; },
+		"mother task");
+		for (auto *first : firsts)
+		{
+			mother_task->must_complete_before(first);
+		}
+
+		addImmediateTask(mother_task);
+		_cv.wait(lock);
+
+		prepareSendJob();
+	}
+	
+	void checkCompletion()
+	{
+		while (_received + _done >= _key2Info.size())
+		{
+			if (_done == _key2Info.size())
+			{
+				std::unique_lock<std::mutex> lock(_result);
+				_cv.notify_one();
+				break;
+			}
+
+			prepareSendJob();
+		}
+	}
+
+	void prepareSendJob()
+	{
+		std::vector<float> everything(_num);
+		
+		int new_receive = 0;
+		for (auto it = _key2Info.begin(); it != _key2Info.end(); it++)
+		{
+			Info &info = it->second;
+			Floats *fs = nullptr;
+			
+			if (info.jobs.size() > 0)
+			{
+				fs = &info.jobs.front();
+			}
+			if (info.done)
+			{
+				fs = &info.best;
+			}
+
+			if (!fs || fs->size() == 0)
+			{
+				continue;
+			}
+			
+			insert_into_main(everything, info, *fs);
+
+			if (info.jobs.size() > 0)
+			{
+				info.jobs.pop();
+
+				if (info.jobs.size() > 0)
+				{
+					new_receive++;
+				}
+			}
+
+		}
+		
+		_received = new_receive;
+		
+		// needs to be able to return lots of [results per key]
+		std::map<Key, float> map = _ref->getMultiResult(everything, this);
+		
+		for (auto it = map.begin(); it != map.end(); it++)
+		{
+			if (_key2Info.count(it->first) == 0)
+			{
+				continue;
+			}
+
+			Info &info = _key2Info.at(it->first);
+			if (info.done)
+			{
+				continue;
+			}
+
+			double score = it->second;
+
+			std::unique_lock<std::mutex> lock(info.expected.mutex());
+			int ticket = info.tickets.front();
+			info.tickets.pop();
+			setScoreForTicket(ticket, score);
+			info.for_collection.push(ticket);
+			info.expected.signal_one();
+		}
+	}
+
+	virtual float getResult(int *job_id, Engine *sender)
+	{
+		Key key = _engine2Key.at(sender);
+		Info &info = _key2Info.at(key);
+		info.expected.wait();
+		std::unique_lock<std::mutex> lock(info.expected.mutex());
+		int ticket = info.for_collection.front();
+		info.for_collection.pop();
+		float score = scoreForTicket(ticket);
+		*job_id = ticket;
+		return score;
+	}
+	
+	virtual int sendJob(const std::vector<float> &all, 
+	                    Engine *sender)
+	{
+		std::unique_lock<std::mutex> lock(_access);
+
+		Key &key = _engine2Key.at(sender);
+		Info &info = _key2Info.at(key);
+
+		if (info.jobs.size() == 0)
+		{
+			_received++;
+		}
+		
+		int ticket = -1;
+		{
+			std::unique_lock<std::mutex> lock(info.expected.mutex());
+			info.expected.expect_one_already_locked();
+			ticket = getNextTicket();
+			info.jobs.push({all});
+			info.tickets.push(ticket);
+		}
+		
+		checkCompletion();
+		
+		return ticket;
+	}
+	
+	virtual void declareDone(Engine *sender, const std::vector<float> &best)
+	{
+		std::unique_lock<std::mutex> lock(_access);
+		_done++;
+		Key &key = _engine2Key.at(sender);
+		_ref->finishedKey(key);
+		Info &info = _key2Info.at(key);
+		info.best = best;
+		info.done = true;
+		checkCompletion();
+	}
+	
+	std::map<Key, float> blueprint()
+	{
+		std::map<Key, float> bp;
+		for (auto it = _key2Info.begin(); it != _key2Info.end(); it++)
+		{
+			const Key &key = it->first;
+			bp[key] = 0;
+		}
+		return bp;
+	}
 private:
+	std::map<Engine *, Key> _engine2Key;
+
+	struct Info
+	{
+		void operator+=(const std::vector<int> &indices)
+		{
+			idxs = indices;
+		}
+		
+		void cleanup()
+		{
+			done = false;
+			jobs = {};
+			best = {};
+			tickets = {};
+			for_collection = {};
+			expected.reset();
+			expected.expect_one();
+		}
+
+		bool done = false;
+		EngineType *engine = nullptr;
+		std::vector<int> idxs;
+		std::queue<Floats> jobs;
+		Floats best;
+		std::queue<int> tickets;
+		std::queue<int> for_collection;
+		ExpectantPhore expected;
+	};
+
+	void insert_into_main(std::vector<float> &everything, 
+	                      const MultiEngine<Key, EngineType>::Info 
+	                      &info,
+	                      const std::vector<float> &fs)
+	{
+		for (int i = 0; i < info.idxs.size() && i < fs.size(); i++)
+		{
+			everything[info.idxs[i]] = fs[i];
+		}
+	}
+	
+
+	std::mutex _access;
+	std::map<Key, Info> _key2Info;
+	int _received = 0;
+	int _done = 0;
+	int _num = 0;
+	int _maxRuns = 10;
+	float _step = 1;
+	std::condition_variable _cv;
+	std::mutex _result;
+	RunsMultiEngine<Key, EngineType> *_ref;
+};
+
+template <class Key>
+class MultiSimplex : public MultiEngine<Key, SimplexEngine>
+{
+public:
+	MultiSimplex(RunsMultiEngine<Key, SimplexEngine> *caller, int param_num)
+	: MultiEngine<Key, SimplexEngine>(caller, param_num)
+	{
+
+	}
 
 };
 
