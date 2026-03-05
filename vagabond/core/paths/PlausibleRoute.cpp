7da371e36fa90421f0c41da96cfed97f18adbfba
Helen Ginn <github@hginn.co.uk>
2026-03-05 12:59:26 +0100

    Max out order in sideChainGradients

--- vagabond/core/paths/PlausibleRoute.cpp
+++ vagabond/core/paths/PlausibleRoute.cpp
@@ -318,6 +318,11 @@ OpSet<ScoreBucket> PlausibleRoute::worstSidechains(int num)
 
 bool PlausibleRoute::sideChainGradients(int order)
 {
+	if (order > _order - 1) 
+	{
+		order = _order - 1;
+	}
+
 	_paramPtrs.clear();
 	_paramStarts.clear();
 	_steps.clear();
@@ -346,7 +351,7 @@ bool PlausibleRoute::sideChainGradients(int order)
 
 		WayPoints &wps = wayPoints(i);
 
-		for (int j = 0; j < order; j++)
+		for (int j = 0; j <= order; j++)
 		{
 			addFloatParameter(&wps._amps[j], step);
 			map[ScoreBucket(parameter(i)->anAtom())].push_back(n);
