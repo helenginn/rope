3eac637c18b9b0486d7f44f320fba1946a91b16b
tGitzHub <56638472+tGitzHub@users.noreply.github.com>
2026-03-13 19:46:57 +0100

    WIP: implement conan as pkgmanager, build succeeded, sdl2 kinks need to be addressed...  /skip-ci

---
+++ conanfile.py
@@ -0,0 +1,26 @@
+from conan import ConanFile
+
+
+class Rope(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "MesonToolchain", "PkgConfigDeps"
+
+    def requirements(self):
+        self.requires("libcurl/[*]")
+        self.requires("fftw/[*]")
+        self.requires("gemmi/[*]")
+        self.requires("glew/[*]")
+        self.requires("glm/[*]")
+        self.requires("nlohmann_json/[*]")
+        self.requires("zlib/[*]")
+
+        if self.settings.os == "Windows":
+            self.requires("sdl/[~2]")
+            self.requires("sdl_image/[~2]")
+
+    def build_requirements(self):
+        if self.settings.os == "Windows":
+            self.tool_requires("pkgconf/[*]")
+
+    def configure(self):
+        self.options["fftw"].enable_float = True
---
+++ recipes/gemmi/conanfile.py
@@ -0,0 +1,37 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+
+
+class GemmiConan(ConanFile):
+    name = "gemmi"
+    version = "0.6.7"
+    license = "Mozilla Public License 2.0"
+    url = "https://github.com/project-gemmi/gemmi"
+    description = "Library for structural biology"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    def source(self):
+        self.run(
+            "git clone --depth 1 --branch v0.6.7 https://github.com/project-gemmi/gemmi.git ."
+        )
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["gemmi_cpp"]
