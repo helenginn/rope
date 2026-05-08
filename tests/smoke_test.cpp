116ddf15701f7420136ceb6f0d385ecb0be0a7fc
tGitzHub <56638472+tGitzHub@users.noreply.github.com>
2026-05-08 14:38:31 +0200

    add OpSet test in smoke_test.cpp

--- tests/smoke_test.cpp
+++ tests/smoke_test.cpp
@@ -1,8 +1,17 @@
 #include <doctest/doctest.h>
+#include <vagabond/utils/OpSet.h>
+#include <vector>
 
 TEST_CASE("smoke: basic arithmetic but wrong") {
   CHECK(2+2==5);
 }
 TEST_CASE("smoke: basic arithmetic but right") {
   CHECK(1+1==2);
+}
+TEST_CASE("Even numbers filtered") {
+  std::vector<int> start = {-1, 2,3,5,6,8};
+  OpSet<int> numbers(start);
+  numbers.filter([](const int &i) {return (i%2==0);});
+  int check =  *numbers.begin();
+  CHECK(check==-1);
 }
