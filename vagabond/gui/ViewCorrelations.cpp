d406a9be04d015fd96181ccb76d57b9c5e8a27e9
Helen Ginn <github@hginn.co.uk>
2026-05-26 17:26:45 +0200

    Remove duplicate unused symbols

--- vagabond/gui/ViewCorrelations.cpp
+++ vagabond/gui/ViewCorrelations.cpp
@@ -82,34 +82,6 @@ void ViewCorrelations::makeList()
 	sb->addSliderIfNeeded();
 }
 
-void removeRow(MatrixXf &matrix, unsigned int rowToRemove)
-{
-	unsigned int numRows = matrix.rows() - 1;
-	unsigned int numCols = matrix.cols();
-
-	if (rowToRemove < numRows)
-	{
-		matrix.block(rowToRemove, 0, numRows - rowToRemove, numCols) 
-		= matrix.bottomRows(numRows - rowToRemove);
-	}
-
-	matrix.conservativeResize(numRows, numCols);
-}
-
-void removeColumn(MatrixXf &matrix, unsigned int colToRemove)
-{
-	unsigned int numRows = matrix.rows();
-	unsigned int numCols = matrix.cols() - 1;
-
-	if (colToRemove < numCols)
-	{
-		matrix.block(0, colToRemove, numRows, numCols - colToRemove) 
-		= matrix.rightCols(numCols - colToRemove);
-	}
-
-	matrix.conservativeResize(numRows, numCols);
-}
-
 void ViewCorrelations::viewAll()
 {
 	OpSet<ProbeTypePair> all;
