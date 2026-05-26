e6376bcd2a6111c54bb9897e51b73cb9b027f07f
Helen Ginn <github@hginn.co.uk>
2026-05-26 08:33:28 +0200

    Remove unused detritus code

--- vagabond/gui/CommunicationAnalysis.cpp
+++ vagabond/gui/CommunicationAnalysis.cpp
@@ -338,62 +338,9 @@ void CommunicationAnalysis::setup()
 	
 	Eigen::JacobiSVD<MatrixXf> svd(mat, Eigen::ComputeFullU | 
 	                               Eigen::ComputeFullV);
-	Eigen::VectorXf w = svd.singularValues();
-	float total = names.size();
 	
-	float shannon = 0;
-	std::cout << "Shannon entropy probs: " << std::endl;
-	for (const float &weight : w)
-	{
-		float prob = weight / total;
-		std::cout << prob << " ";
-		shannon -= prob * log(prob) / log(2);
-	}
-	std::cout << std::endl;
 
-	Text *text = new Text("Shannon entropy: " + f_to_str(shannon, 3));
-	text->setCentre(0.5, 0.87);
-	addObject(text);
 
-	Eigen::MatrixXf u = svd.matrixU();
-	
-	float average_dot = 0;
-	float num = 0;
-	auto name = names.begin();
-	for (int i = 0; i < u.rows(); i++)
-	{
-		Eigen::VectorXf v1 = mat.row(i);
-		for (int j = i; j < u.rows(); j++)
-		{
-			Eigen::VectorXf v2 = mat.row(j);
-
-			float dot = 0;
-			for (int k = 0; k < v1.size(); k++)
-			{
-				float weight = w.size() > k ? w(k) : 0.f;
-				weight = 1;
-				dot += v1(k) * v2(k) * weight;
-			}
-//			std::cout << dot << " ";
-			float weight = 1;//(1 - fabs(dot));
-			average_dot += dot * weight;
-			num += weight;
-		}
-		std::cout << *name << ", ";
-		name++;
-		for (int j = 0; j < v1.size(); j++)
-		{
-			std::cout << v1(j) * w(j) << ", ";
-//			std::cout << mat(i, j) * w(j) << " ";
-		}
-		std::cout << std::endl;
-	}
-
-	average_dot /= num;
-	std::cout << "Average dot: " << average_dot << std::endl;
-	text = new Text("Average dot product: " + f_to_str(average_dot, 3));
-	text->setCentre(0.5, 0.93);
-	addObject(text);
 	
 	Box *box = new Box();
 
