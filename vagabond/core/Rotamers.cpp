b840f145ae9e5fd54a5a15f01a587eac354a2546
Romain <romain.vin100@gmail.com>
2026-05-29 15:46:57 +0200

    Creation of Rotamers, to be able to follow the chi angle for specific rotamers

---
+++ vagabond/core/Rotamers.cpp
@@ -0,0 +1,74 @@
+//
+// Created by romain on 29/04/2026.
+//
+
+#include "Rotamers.h"
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <iostream>
+
+RotamerLibrary::RotamerLibrary()
+{
+    _allRotamers = loadRotamersFromJson(_filepath);
+    std::cout << "All rotamers: " << _allRotamers.size() << '\n';
+}
+
+std::map<std::string,std::vector<Rotamer>> RotamerLibrary::loadRotamersFromJson(std::string const &filepath)
+{
+    std::ifstream f;
+    f.open(filepath);
+
+    //std::ifstream file(filepath);
+    if (!f.is_open())
+    {
+        throw std::runtime_error("Could not open rotamer JSON: " + filepath);
+    }
+
+    nlohmann::json j;
+    f >> j;
+    f.close();
+
+    std::vector<Rotamer> rotamers;
+    std::map<std::string,std::vector<Rotamer>> allRotamers;
+
+    for (auto &[resname, entries] : j.items())
+    {
+        for (auto &entry : entries)
+        {
+            Rotamer r;
+            r.id = entry["id"];
+            r.name = entry["name"];
+            r.residue = resname;
+
+            // parse chi angles dynamically (chi_1 to chi_4)
+            for (int i = 1; i <= 4; i++)
+            {
+                std::string chiKey = "chi_" + std::to_string(i);
+                std::string widthKey = "chi-width_" + std::to_string(i);
+
+                if (entry.contains(chiKey))
+                {
+                    r.chi.push_back(entry[chiKey].is_null() ? 0.0f // 0 == no chi value
+                                                               : entry[chiKey].get<float>());
+                    r.chi_width.push_back(entry[widthKey].is_null() ? -1.0f //
+                                                               : entry[widthKey].get<float>());
+                }
+            }
+            rotamers.push_back(r);
+        }
+        allRotamers[resname] = rotamers;
+        rotamers.clear();
+    }
+
+    return allRotamers;
+}
+
+std::vector<Rotamer> RotamerLibrary::rotamersForResidues(std::string resName)
+{
+    std::vector<Rotamer> result;
+    for (const Rotamer &r : _allRotamers[resName])
+    {
+        result.push_back(r);
+    }
+    return result;
+}
---
+++ vagabond/core/Rotamers.h
@@ -0,0 +1,36 @@
+//
+// Created by romain on 29/04/2026.
+//
+#ifndef __vagabond__Rotamers__
+#define __vagabond__Rotamers__
+#include <string>
+#include <vector>
+#include <map>
+struct Rotamer
+{
+    int id{};
+    std::string name;
+    std::string residue;
+    std::vector<float> chi;
+    std::vector<float> chi_width;
+};
+struct RotamerMap
+{
+    bool loaded {false};
+    float initialAngle {};
+    int RotamerValue {0};  // store the rotamer number present in the penultimate_lib. if rotamer number = 0, initialChiAngles are loaded
+};
+class RotamerLibrary //: public Rotamer
+{
+public:
+    RotamerLibrary();
+    std::map<std::string,std::vector<Rotamer>> loadRotamersFromJson(std::string const &filepath);
+    std::vector<Rotamer> rotamersForResidues(std::string resName);
+    std::map<std::string, std::vector<Rotamer>> _allRotamers {};
+
+
+private:
+    std::string const _filepath = "../assets/geometry/penultimate_lib.json";
+};
+
+#endif
