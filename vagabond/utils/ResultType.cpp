fbee6a03cce28f5023809a8516c4f468cc9bf2a7
tGitzHub <56638472+tGitzHub@users.noreply.github.com>
2026-08-13 00:21:14 +0200

    add Result<void, E> logic + tests for it

--- vagabond/utils/ResultType.cpp
+++ vagabond/utils/ResultType.cpp
@@ -120,5 +120,85 @@ TEST_CASE("Result - map/map_err pass-through") {
     CHECK(mapped.unwrap() == 5);
   }
 }
+TEST_CASE("Result<void,E> - Basic Ok operations") {
+  Result<void, std::string> res = Ok();
+  REQUIRE(res.is_ok());
+  CHECK(!res.is_err());
+  CHECK_NOTHROW(res.unwrap());
+}
 
+TEST_CASE("Result<void,E> - Basic Err operations") {
+  Result<void, std::string> res = Err(std::string("failed"));
+  REQUIRE(res.is_err());
+  CHECK(!res.is_ok());
+  CHECK(res.unwrap_err() == "failed");
+}
+
+TEST_CASE("Result<void,E> - Throws on invalid access") {
+  Result<void, std::string> ok_res = Ok();
+  Result<void, std::string> err_res = Err(std::string("Error"));
+  CHECK_THROWS_AS(err_res.unwrap(), std::runtime_error);
+  CHECK_THROWS_AS(ok_res.unwrap_err(), std::runtime_error);
+}
+
+TEST_CASE("Result<void,E> - expect()/expect_err()") {
+  SUBCASE("expect on Err with HasToString") {
+    Result<void, CustomTestError> res = Err(CustomTestError{"boom"});
+    CHECK_THROWS_AS(std::move(res).expect("Expected success"),
+                    std::runtime_error);
+  }
+  SUBCASE("expect on Err without HasToString") {
+    Result<void, PlainTestError> res = Err(PlainTestError{});
+    CHECK_THROWS_AS(std::move(res).expect("Expected success"),
+                    std::runtime_error);
+  }
+  SUBCASE("expect_err on Ok") {
+    Result<void, std::string> res = Ok();
+    CHECK_THROWS_AS(std::move(res).expect_err("Expected failure"),
+                    std::runtime_error);
+  }
+}
+
+TEST_CASE("Result<void,E> - map/map_err") {
+  SUBCASE("map on Ok produces value") {
+    Result<void, std::string> res = Ok();
+    auto mapped = std::move(res).map([]() { return 42; });
+    CHECK(mapped.unwrap() == 42);
+  }
+  SUBCASE("map on Err passes through") {
+    Result<void, std::string> res = Err(std::string("bad"));
+    auto mapped = std::move(res).map([]() { return 42; });
+    CHECK(mapped.is_err());
+    CHECK(mapped.unwrap_err() == "bad");
+  }
+  SUBCASE("map_err on Err transforms error") {
+    Result<void, std::string> res = Err(std::string("bad"));
+    auto mapped = std::move(res).map_err([](std::string s) { return s + "!"; });
+    CHECK(mapped.unwrap_err() == "bad!");
+  }
+  SUBCASE("map_err on Ok passes through") {
+    Result<void, std::string> res = Ok();
+    auto mapped = std::move(res).map_err([](std::string s) { return s + "!"; });
+    CHECK(mapped.is_ok());
+    CHECK_NOTHROW(mapped.unwrap());
+  }
+}
+TEST_CASE("Result - map() with void-returning func") {
+  SUBCASE("map on Ok with void func transitions to Result<void,E>") {
+    Result<int, std::string> res = Ok(5);
+    int captured = 0;
+    auto mapped = std::move(res).map([&captured](int x) { captured = x; });
+    CHECK(captured == 5);
+    CHECK(mapped.is_ok());
+    CHECK_NOTHROW(mapped.unwrap());
+  }
+  SUBCASE("map on Err with void func passes error through") {
+    Result<int, std::string> res = Err(std::string("bad"));
+    bool called = false;
+    auto mapped = std::move(res).map([&called](int) { called = true; });
+    CHECK(!called);
+    CHECK(mapped.is_err());
+    CHECK(mapped.unwrap_err() == "bad");
+  }
+}
 #endif
--- vagabond/utils/ResultType.h
+++ vagabond/utils/ResultType.h
@@ -1,19 +1,19 @@
 // vagabond
 // Copyright (C) 2022 Helen Ginn
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-// 
+//
 // Please email: vagabond @ hginn.co.uk for more details.
 
 #pragma once
@@ -34,10 +34,14 @@ concept HasToString = requires(T obj) {
 template <typename T> struct Ok {
   T value;
 };
+template <typename T> Ok(T) -> Ok<T>;
 
 template <typename E> struct Err {
   E value;
 };
+
+template <> struct Ok<void> {};
+Ok() -> Ok<void>;
 
 template <typename T, typename E> class [[nodiscard]] Result {
   std::variant<T, E> data_;
@@ -144,10 +148,18 @@ public:
 
   template <typename F> auto map(F &&func) && {
     using U = std::invoke_result_t<F, T>;
-    if (is_ok()) {
-      return Result<U, E>(Ok(func(std::get<0>(std::move(data_)))));
+    if constexpr (std::is_void_v<U>) {
+      if (is_ok()) {
+        func(std::get<0>(std::move(data_)));
+        return Result<void, E>(Ok<void>{});
+      }
+      return Result<void, E>(Err(std::get<1>(std::move(data_))));
+    } else {
+      if (is_ok()) {
+        return Result<U, E>(Ok(func(std::get<0>(std::move(data_)))));
+      }
+      return Result<U, E>(Err(std::get<1>(std::move(data_))));
     }
-    return Result<U, E>(Err(std::get<1>(std::move(data_))));
   }
 
   template <typename F> auto map_err(F &&func) && {
@@ -156,5 +168,83 @@ public:
       return Result<T, U>(Err(func(std::get<1>(std::move(data_)))));
     }
     return Result<T, U>(Ok(std::get<0>(std::move(data_))));
+  }
+};
+
+template <typename E> class [[nodiscard]] Result<void, E> {
+  std::variant<std::monostate, E> data_;
+
+public:
+  Result(Ok<void>) : data_(std::in_place_index<0>) {}
+
+  template <typename U = E>
+    requires std::convertible_to<U, E>
+  Result(Err<U> err) : data_(std::in_place_index<1>, std::move(err.value)) {}
+
+  [[nodiscard]] bool is_ok() const { return data_.index() == 0; }
+  [[nodiscard]] bool is_err() const { return data_.index() == 1; }
+
+  void unwrap() const {
+    if (is_err())
+      throw std::runtime_error("Called unwrap on an Err value!");
+  }
+
+  E &unwrap_err() & {
+    if (is_ok())
+      throw std::runtime_error("Called unwrap_err on an Ok value!");
+    return std::get<1>(data_);
+  }
+
+  E unwrap_err() && {
+    if (is_ok())
+      throw std::runtime_error("Called unwrap_err on an Ok value!");
+    return std::get<1>(std::move(data_));
+  }
+
+  const E &unwrap_err() const & {
+    if (is_ok())
+      throw std::runtime_error("Called unwrap_err on an Ok value!");
+    return std::get<1>(data_);
+  }
+
+  void expect(const std::string &msg) &&
+    requires HasToString<E>
+  {
+    if (is_err()) {
+      throw std::runtime_error(msg + ": " + error().toString());
+    }
+  }
+
+  void expect(const std::string &msg) &&
+    requires(!HasToString<E>)
+  {
+    if (is_err()) {
+      throw std::runtime_error(msg);
+    }
+  }
+
+  E expect_err(const std::string &msg) && {
+    if (is_ok()) {
+      throw std::runtime_error(msg);
+    }
+    return std::get<1>(std::move(data_));
+  }
+
+  const E &error() const { return std::get<1>(data_); }
+
+  template <typename F> auto map(F &&func) && {
+    using U = std::invoke_result_t<F>;
+    if (is_ok()) {
+      return Result<U, E>(Ok(func()));
+    }
+    return Result<U, E>(Err(std::get<1>(std::move(data_))));
+  }
+
+  template <typename F> auto map_err(F &&func) && {
+    using U = std::invoke_result_t<F, E>;
+    if (is_err()) {
+      return Result<void, U>(Err(func(std::get<1>(std::move(data_)))));
+    }
+    return Result<void, U>(Ok<void>{});
   }
 };
