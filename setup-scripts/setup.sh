c010a42c0c060edbe09818d73bb4df8046b6fe77
tGitzHub <56638472+tGitzHub@users.noreply.github.com>
2026-09-14 14:09:40 +0200

    Change symlink and Junction to copying assets in setup-scripts

--- setup-scripts/setup.sh
+++ setup-scripts/setup.sh
@@ -303,24 +303,24 @@ else
 fi
 meson compile -C "$BUILDDIR"
 
-info "Symlinking assets into build dir..."
+# -- Copy Assets --
+info "Copying assets into build dir..."
 
-mkdir -p "$BUILDDIR/assets"
+assetRoot="$BUILDDIR/assets"
+mkdir -p "$assetRoot"
+
 for asset in shaders images geometry; do
-  source_asset="$asset"
-  [ "$asset" = shaders ] && source_asset=shaders_450
-  asset_target="$SRCDIR/assets/$source_asset"
-  asset_link="$BUILDDIR/assets/$asset"
-
-  if [ -L "$asset_link" ] &&
-     [ "$(readlink "$asset_link")" = "$asset_target" ]; then
-  continue
+  if [[ "$asset" == "shaders" ]]; then
+    sourceAsset="shaders_450"
+  else
+    sourceAsset="$asset"
   fi
 
-  if [ -e "$asset_link" ] || [ -L "$asset_link" ]; then
-     die "Asset path already exists with different contents: $asset_link"
-  fi
-  ln -s "$asset_target" "$asset_link"
+  assetSource="$SRCDIR/assets/$sourceAsset"
+  assetTarget="$assetRoot/$asset"
+
+  mkdir -p "$assetTarget"
+  cp -R "$assetSource"/. "$assetTarget"/
 done
 
 if $USE_CLANGD; then
